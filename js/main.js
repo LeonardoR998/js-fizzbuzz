8b10d0e56ed2f8206d669d18032da2ecc1dd4f52
LeonardoR998 <leonardoricci839@gmail.com>
2024-10-15 14:55:07 +0200

    add data

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@
 // Scrivi un programma che stampi i numeri da 1 a 100,
 // ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
 
-// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.Prima di partire a scrivere codice poniamoci qualche domanda:
+// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+// Prima di partire a scrivere codice poniamoci qualche domanda:
 
 // Come faccio a sapere se un numero è divisibile per?
 
@@ -17,4 +18,22 @@
 
 // ! ELABORAZIONE //
 
-// ! OUTPUT //
+// stampo i numeri da 1 a 100
+
+const numberOfRepetions = 100;
+
+for (let i = 0; i < numberOfRepetions; i++) {
+  console.log(`Ripetizione ${i + 1}: indice ${i} `);
+
+  //SE il numero è multiplo di 3 stampa Fizz
+  if (i % 3 === 0) {
+    console.log("Fizz");
+    //ALTRIMENTI SE il numero è multiplo di 5 stampa Buzz
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  }
+  // SE i numeri non sono divisibili per 3 o 5 stampa FizzBuzz
+  if (!(i % 3 === 0 && i % 5 === 0)) {
+    console.log("FizzBuzz");
+  }
+}
